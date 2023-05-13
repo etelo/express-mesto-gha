269ecbbe4b3cc8b89b9a3664356be1feff04f363
Alex Prc <pralig@yandex.ru>
2023-05-13 18:06:53 +0300

    14пр

--- models/user.js
+++ models/user.js
@@ -3,49 +3,49 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 const NotAuthorized = require("../errors/not-authorized");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      minlength: 2,
-      maxlength: 30,
-      default: "Жак-Ив Кусто",
-    },
-    about: {
-      type: String,
-      minlength: 2,
-      maxlength: 30,
-      default: "Исследователь",
-    },
-    avatar: {
-      type: String,
-      minlength: 2,
-      default:
-        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
-      validate: [
-        {
-          validator: (v) => /^https?:\/\/(?:www\.)?\w[\w\-.~:/?#[\]@!$&'()*+,;=]+(?:#\w+)?$/.test(v),
-          message: "Неправильный адрес ссылки",
-        },
-      ],
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: (v) => validator.isEmail(v),
-        message: "Неверный формат email",
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: "Жак-Ив Кусто",
+  },
+  about: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: "Исследователь",
+  },
+  avatar: {
+    type: String,
+    minlength: 2,
+    default:
+      "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
+    validate: [
+      {
+        validator: (v) =>
+          /^https?:\/\/(?:www\.)?\w[\w\-.~:/?#[\]@!$&'()*+,;=]+(?:#\w+)?$/.test(
+            v
+          ),
+        message: "Неправильный адрес ссылки",
       },
-    },
-    password: {
-      type: String,
-      required: true,
-      select: false,
+    ],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: "Неверный формат email",
     },
   },
-  { versionKey: false }
-);
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+});
 
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email })
